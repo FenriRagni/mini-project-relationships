72bf676778bf3f27d0c39f8d72c6842bb421b837
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-11-02 14:07:18 -0400

    add connections.js, schema.sql, routes for travelers

---
+++ routes/api/travelerRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Traveler, Location, Trips } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const travelerData = await Traveler.findAll();
+        res.status(200).json(travelerData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try{
+        const travelerData = await Traveler.create(req.body);
+        res.status(201).json(travelerData);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try{
+        const travelerData = await Traveler.findByPK(req.params.id);
+        if(!travelerData){
+            res.status(404).json({ message: 'No traveler found with that id!'})
+            return;
+        }
+        res.status(200).json(travelerData);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const travelerData = await Traveler.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if(!travelerData) {
+            res.status(404).json({ message: 'No traveler found with that id!'});
+            return;
+     
+        }
+        res.status(200).json(travelerData);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
